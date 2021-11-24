69536ca3611a5db42f8cd0c7788fb90f4a8ecf4b
mali0303 <trustable_ali@live.com>
2021-11-24 11:27:47 +0000

    circle class

--- main.py
+++ main.py
@@ -2,15 +2,38 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import os
+from math import pi
+
+class Circle:
+    def __init__(self, radius, fill ='red', stroke='black'):
+        self._radius = radius
+        self._fill = fill
+        self._stroke = stroke
+
+    def calculate_area(self):
+        return pi * self._radius ** 2
+
+    @property
+    def radius(self):
+        return self._radius
+
+    def __len__(self):
+        return 2 * pi * self._radius
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def main():
+    circle = Circle(5.0, fill='orange', stroke='red')
+    print(f"area = {circle.calculate_area()}")
+
+    circle2 = Circle(8.0)
+    return 0 # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    sys.exit(main())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
